c72c23431641bd08e57346505b3f88954ac96ed6
jjh <475890319@qq.com>
2021-07-03 19:05:40 +0800

    详情开发

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
-    <main-tab-bar/>
+    <keep-alive exclude="Detail">
+      <router-view />
+    </keep-alive>
+    <main-tab-bar />
   </div>
 </template>
 
 <script>
-import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    MainTabBar
-  }
-}
+    MainTabBar,
+  },
+};
 </script>
 
 <style>
